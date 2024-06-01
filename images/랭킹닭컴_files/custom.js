9c3f226cd9d4d7529c78342f0f706acf0447b904
KimJunHo98 <dydgnscc6845@gmail.com>
2024-06-01 21:25:46 +0900

    240601 header, sidemenu 구축

--- images/랭킹닭컴_files/custom.js
+++
@@ -1,9 +0,0 @@
-const category = document.querySelector(".category");
-const sideMenu = document.querySelector("#side_menu");
-
-// 슬라이드 사이드메뉴
-const handleClickSideMenu = () => {
-    sideMenu.classList.toggle("active");
-}
-
-category.addEventListener("click", handleClickSideMenu);
--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,25 @@
-const category = document.querySelector(".category");
 const sideMenu = document.querySelector("#side_menu");
+const sideMenuBtn = document.querySelector(".side_menu_btn");
+const sideMenuLink = document.querySelector(".side_menu_link");
+const sideMenuDepth = document.querySelector(".side_menu_depth");
 
 // 슬라이드 사이드메뉴
-const handleClickSideMenu = () => {
-    sideMenu.classList.toggle("active");
+function handleEnterSideMenu() {
+    sideMenu.classList.add("active");
+}
+function handleLeaveSideMenu() {
+    sideMenu.classList.remove("active");
 }
 
-category.addEventListener("click", handleClickSideMenu);
+// 사이드 메뉴 링크 토글 글씨색상
+function handleEnterSideMenuDepth() {
+    sideMenuLink.classList.add("depth_hover");
+};
+function handleLeaveSideMenuDepth() {
+    sideMenuLink.classList.remove("depth_hover");
+};
+
+sideMenuBtn.addEventListener("pointerenter", handleEnterSideMenu);
+sideMenuBtn.addEventListener("pointerleave", handleLeaveSideMenu);
+sideMenuDepth.addEventListener("pointerenter", handleEnterSideMenuDepth)
+sideMenuDepth.addEventListener("pointerleave", handleLeaveSideMenuDepth)
