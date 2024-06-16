ca48a31356cee4f2a57532acc6366629b8a1de75
KimJunHo98 <dydgnscc6845@gmail.com>
2024-06-16 20:25:11 +0900

    240615 탭메뉴 완료, 인기 카테고리 탭메뉴 완료, 스킵메뉴 완료

--- js/tab.js
+++ js/tab.js
@@ -1,6 +1,9 @@
 const tabLinks = document.querySelectorAll(".tab_nav_link");
 const tabMenuItems = document.querySelectorAll(".tabmenu_item");
+const cgTabLinks = document.querySelectorAll(".category_tab_link");
+const cgTabMenuItems = document.querySelectorAll(".category_tabmenu_item");
 
+// 테마별 상품
 tabLinks.forEach(tabLink => {
     tabLink.addEventListener("click", function (e) {
         const tabId = tabLink.getAttribute("href").replace("#", "");
@@ -15,6 +18,28 @@ tabLinks.forEach(tabLink => {
 
         // 해당 탭 링크의 href 속성 값과 일치하는 탭 메뉴 아이템을 찾아 active 클래스 추가 및 제거
         tabMenuItems.forEach(item => {
+            if (item.id === tabId) {
+                item.classList.add("active");
+            } else {
+                item.classList.remove("active");
+            }
+        });
+    });
+});
+
+// 인기 카테고리 상품
+cgTabLinks.forEach(tabLink => {
+    tabLink.addEventListener("click", function (e) {
+        const tabId = tabLink.getAttribute("href").replace("#", "");
+        
+        e.preventDefault();
+
+        cgTabLinks.forEach(link => {
+            link.classList.remove("active");
+        });
+        tabLink.classList.add("active");
+
+        cgTabMenuItems.forEach(item => {
             if (item.id === tabId) {
                 item.classList.add("active");
             } else {
