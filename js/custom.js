527a396dfa80a65f18fa6d3daf2679eba22afdcc
KimJunHo98 <dydgnscc6845@gmail.com>
2024-06-17 19:52:56 +0900

    240617 푸터 작업 완료

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,9 @@ const sideMenu = document.getElementById("sideMenu");
 const sideMenuBtn = document.querySelector(".side_menu_btn");
 const sideMenuLink = document.querySelector(".side_menu_link");
 const sideMenuDepth = document.querySelector(".side_menu_depth");
+// 사이드 퀵메뉴
+const sideQm = document.getElementById("sideQuickMenu");
+const topBtn = document.querySelector(".top");
 // 푸터
 const selectMenu = document.querySelector(".select_menu");
 const upIco = document.querySelector(".up_ico");
@@ -47,15 +50,36 @@ function pageScrollBlock() {
     body.style.overflow = "auto";
 }
 
+// 사이드 퀵메뉴
+function sideQmPosition(){
+    const scrollTop = window.scrollY;
+    // const offSetTop = headerGnb.offsetTop;
+
+    if(scrollTop >= 300){
+        sideQm.classList.add("fixed");
+    }else{
+        sideQm.classList.remove("fixed");
+    }
+}
 function handleClickFmBtn() {
     selectMenu.classList.toggle("active");
     upIco.classList.toggle("rotate");
 }
 
+// 상단 이동
+function handleClickTopBtn(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
 window.addEventListener("scroll", scrollStickyHeader);
+window.addEventListener("scroll", sideQmPosition);
 fmBtn.addEventListener("click", handleClickFmBtn);
 sideMenuBtn.addEventListener("click", handleEnterSideMenu);
 sideMenu.addEventListener("pointerenter", pageScrollNone);
 sideMenu.addEventListener("pointerleave", pageScrollBlock);
 sideMenuDepth.addEventListener("pointerenter", handleEnterSideMenuDepth);
 sideMenuDepth.addEventListener("pointerleave", handleLeaveSideMenuDepth);
+topBtn.addEventListener("click", handleClickTopBtn);
